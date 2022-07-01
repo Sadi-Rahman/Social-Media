9cc1334633ff55ac80d92c0ce9b5dcfa96785804
Sadi-Rahman <sadiakash1309@gmail.com>
2022-07-01 20:35:00 +0600

    Takes all the input form the from

--- regCheck.php
+++ regCheck.php
@@ -3,12 +3,14 @@
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
 $email = $_REQUEST['email'];
+$tel = $_REQUEST['tel'];
+$address = $_REQUEST['address']; 
 
 if ($username == null || $password == null || $email == null) {
     echo "Invalid Username/Password/Email !!!";
 }
 else {
-   $data = $username."|".$password."|".$email."\r\n";
+   $data = $username."|".$password."|".$email."|".$tel."|".$address."\r\n";
    $file =fopen ('user.txt','a');
    fwrite($file,$data);
     header('location: login.html');
