471877e90fb966f49ec6e8ea5b897ddd6baa9f26
Sadi-Rahman <sadiakash1309@gmail.com>
2022-07-02 19:41:59 +0600

    Added home.php

---
+++ home.php
@@ -0,0 +1,40 @@
+<?php 
+	if(!isset($_COOKIE['status'])){
+		header('location: login.html');
+	}
+?>
+
+<html>
+  <head>
+    <title>Homepage</title>
+  </head>
+
+  <body>
+    <h1>Welcome to the Homepage</h1>
+    <a href="profile.html">Profile</a><br />
+    <a href="about.html">About Us</a><br />
+    <a href="logout.html">Logout</a><br />
+
+<?php 
+				$file = fopen('post.txt', 'r');
+				while(!feof($file)){
+					$post = fgets($file);
+					$post = ($post);
+					//print_r($user);
+					
+			?>
+    <tr>
+				<th><?=$post?></th>
+				
+			
+	 
+				</th>
+			</tr>
+			<?php
+					}
+				
+			?>
+
+
+  </body>
+</html>
