74f3c65086554c9419e33847772bf8c2487b2a22
Sadi-Rahman <sadiakash1309@gmail.com>
2022-07-03 22:58:50 +0600

    Profile

---
+++ profile.php
@@ -0,0 +1,63 @@
+<?php 
+	if(!isset($_COOKIE['status'])){
+		header('location: login.html');
+	}
+?>
+
+<html>
+  <head>
+    <title>Profile Page</title>
+  </head>
+
+  <body>
+    <h1>Welcome to the Profile Page</h1>
+    <br />
+    <a href="index.html">Homepage</a> | <a href="about.html">About Us</a> |
+    <a href="logout.html">Logout</a> <br />
+    <br />
+
+    <form>
+      <tr>
+        <td>Upload Profile Picture</td>
+        <br /><br />
+        <input type="file" id="myFile" name="filename" /><br /><br />
+        <input type="submit" value="Upload" /><br>
+
+      <?php 
+				$file = fopen('user.txt', 'r');
+				while(!feof($file)){
+					$user = fgets($file);
+					$user = explode("|", $user);
+					//print_r($user);
+					if(count($user) > 1){
+			?>
+      
+				
+      <?php
+
+          }
+        }
+          ?>
+
+        <hr />
+        <br /><br />
+      </tr>
+    </form>
+    <form action="post.php" method="post" enctype="">
+      <tr>
+        <td>
+          <textarea
+            name="ques"
+            placeholder="Write Questions or your thoughts"
+            id=""
+            cols="50"
+            rows="7"
+          ></textarea
+          ><br /><br />
+        </td>
+        <td><input type="submit" value="Post" /></td>
+        <hr />
+      </tr>
+    </form>
+  </body>
+</html>
