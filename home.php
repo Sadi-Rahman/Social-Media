8697cb9347c8b60e57eecb5bd2f5f865d31637b9
Sadi-Rahman <sadiakash1309@gmail.com>
2022-07-03 23:42:20 +0600

    Added Comment

--- home.php
+++ home.php
@@ -1,8 +1,3 @@
-<?php 
-	if(!isset($_COOKIE['status'])){
-		header('location: login.html');
-	}
-?>
 
 <html>
   <head>
@@ -13,9 +8,9 @@
     <h1>Welcome to the Homepage</h1>
     <a href="profile.php">Profile</a><br />
     <a href="about.html">About Us</a><br />
-    <a href="logout.html">Logout</a><br />
+    <a href="logout.html">Logout</a><br /><br><br>
 
-<?php 
+			<?php 
 				$file = fopen('post.txt', 'r');
 				while(!feof($file)){
 					$post = fgets($file);
@@ -23,18 +18,26 @@
 					//print_r($user);
 					
 			?>
-    <tr>
-				<th><?=$post	?></th>
-				
-			
-	 
-				</th>
+			<table  border="1px" style="border-collapse: collapse">
+    		<tr>
+			<td><?=$post	?></td><br>
 			</tr>
+			<form action="comment.php" method="post" enctype="">
+			<tr>
+			<td>
+			<textarea placeholder="Comment" name="comment" id="" cols="30" rows="1"></textarea>
+			<input type="submit" name="Post" value="Post" id="">
+			</td></tr>
+			</form>
 			<?php
 					}
 				
 			?>
 
+			<tr>
+				
+			</tr>
+</table>
 
   </body>
 </html>
